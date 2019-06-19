62d82d522c0d74196b2c945f5696644f4675f87c
Sid Hussmann <sid.hussmann@gapfruit.com>
2019-06-19 15:24:11 +0200

    sd_card_drv: add support for i.MX6Q Apalis

--- repos/os/src/drivers/sd_card/spec/imx6q_apalis/target.mk
+++ repos/os/src/drivers/sd_card/spec/imx6q_apalis/target.mk
@@ -1,6 +1,5 @@
 TARGET   = imx6q_apalis_sd_card_drv
 REQUIRES = arm_v7
-REQUIRES = imx6q_apalis
 SRC_CC  += adma2.cc spec/imx/driver.cc spec/imx6/driver.cc
 INC_DIR  = $(REP_DIR)/src/drivers/sd_card/spec/imx
 
