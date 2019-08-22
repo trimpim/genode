e2f545acffe0648aece1b5d8f867cdfba1a559ee
Pirmin Duss <pirmin.duss@gapfruit.com>
2019-08-22 11:39:30 +0200

    fixup! arm: enable platform base-linux
    Without this patch building of the linux_nic_drv depot package fails.

--- repos/base-linux/lib/import/import-syscall-linux.mk
+++ repos/base-linux/lib/import/import-syscall-linux.mk
@@ -1,9 +1,10 @@
 HOST_INC_DIR += $(dir $(call select_from_repositories,src/lib/syscall/linux_syscalls.h))
+IMPORT_DIR   := $(dir $(call select_from_repositories,lib/import/import-syscall-linux.mk))
 
 ifeq ($(filter-out $(SPECS),x86),)
-	include $(REP_DIR)/lib/import/spec/x86/import-syscall-linux.mk
+	include $(IMPORT_DIR)/spec/x86/import-syscall-linux.mk
 endif
 
 ifeq ($(filter-out $(SPECS),arm_v7a),)
-	include $(REP_DIR)/lib/import/spec/arm_v7a/import-syscall-linux.mk
+	include $(IMPORT_DIR)/spec/arm_v7a/import-syscall-linux.mk
 endif
