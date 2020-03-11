cf0e9b52b49f96866c07bcd8102d79a991239dfc
Pirmin Duss <pirmin.duss@gapfruit.com>
2020-03-11 08:32:39 +0100

    base-linux: SYS_ioperm/SYS_iopl not supported on arm
    Issue #3691

--- repos/base-linux/src/core/include/core_linux_syscalls.h
+++ repos/base-linux/src/core/include/core_linux_syscalls.h
@@ -76,15 +76,19 @@ inline int lx_stat(const char *path, struct stat64 *buf)
  ** Functions used by core's io port session support code **
  ***********************************************************/
 
+#ifdef SYS_ioperm
 inline int lx_ioperm(unsigned long from, unsigned long num, int turn_on)
 {
 	return lx_syscall(SYS_ioperm, from, num, turn_on);
 }
+#endif
 
+#ifdef SYS_iopl
 inline int lx_iopl(int level)
 {
 	return lx_syscall(SYS_iopl, level);
 }
+#endif
 
 /**************************************************
  ** Functions used by core's io mem session code **
