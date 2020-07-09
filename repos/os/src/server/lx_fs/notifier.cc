2526cefe55360bab0b8e9b89272ea5b89a0500e2
Pirmin Duss <pirmin@trimpim.ch>
2020-07-09 09:49:54 +0200

    lx_fs: enable session quota upgrades
    Issue #4070

--- repos/os/src/server/lx_fs/notifier.cc
+++ repos/os/src/server/lx_fs/notifier.cc
@@ -32,7 +32,8 @@ namespace Lx_fs
 	enum {
 		STACK_SIZE     = 8 * 1024,
 		EVENT_SIZE     = (sizeof (struct inotify_event)),
-		EVENT_BUF_LEN  = (1024 * (EVENT_SIZE + NAME_MAX + 1))
+		EVENT_BUF_LEN  = (1024 * (EVENT_SIZE + NAME_MAX + 1)),
+		PARALELL_NOTIFICATIONS = 4,
 	};
 }
 
@@ -63,7 +64,7 @@ void Lx_fs::Notifier::_add_to_watched(char const *path)
 int Lx_fs::Notifier::_add_cap(char const *path, Signal_context_capability cap)
 {
 	int wd { -1 };
-	auto add_fn = [&] (Entry &elem) {
+	auto add_fn = [&cap, &path, &wd, this] (Entry &elem) {
 		if (elem.path == path) {
 			Cap_entry *c { new (_heap) Cap_entry { cap } };
 			elem.add_capabilty(c);
@@ -79,10 +80,55 @@ int Lx_fs::Notifier::_add_cap(char const *path, Signal_context_capability cap)
 }
 
 
+void Lx_fs::Notifier::_add_notify(Signal_context_capability cap)
+{
+	for (auto const *e = _notify_queue.first(); e != nullptr; e = e->next()) {
+		if (e->cap == cap) {
+			return;
+		}
+	}
+
+	auto *entry { new (_heap) Cap_entry { cap } };
+	_notify_queue.insert(entry);
+
+	if (!_notify_timer_running) {
+		_notify_timer_running = true;
+		_notify_timer.trigger_once(5000);
+	}
+}
+
+
+void Lx_fs::Notifier::_process_notify()
+{
+	_notify_timer_running = false;
+
+	/**
+	 * limit amount of watch events sent at the same time,
+	 * to prevent an overflow of the packet ack queue of the
+	 * File_system session.
+	 */
+	int cnt { 0 };
+	auto *e = _notify_queue.first();
+	while ((e != nullptr) && (cnt < PARALELL_NOTIFICATIONS)) {
+		Signal_transmitter {e->cap}.submit();
+		++cnt;
+
+		auto *tmp = e;
+		e = e->next();
+		_notify_queue.remove(tmp);
+	}
+
+	if (_notify_queue.first() != nullptr) {
+		_notify_timer_running = true;
+		_notify_timer.trigger_once(5000);
+	}
+}
+
+
 Lx_fs::Notifier::Notifier(Env &env)
 :
 	Thread { env, "inotify", STACK_SIZE },
-	_heap { env.ram(), env.rm() }
+	_env   { env }
 {
 	_fd = inotify_init();
 
@@ -101,13 +147,12 @@ Lx_fs::Notifier::~Notifier()
 		Entry *r = e;
 		e = e->next();
 
-		auto rm_cap_fn = [&] (Cap_entry &c) {
-			r->remove_capability(&c);
-			destroy(_heap, &c);
-		};
+
+		r->remove_all(_heap, [ ] (Signal_context_capability cap) {
+			Signal_transmitter {cap}.submit();
+		});
 
 		_watched_nodes.remove(r);
-		r->for_each_capability(rm_cap_fn);
 
 		destroy(_heap, r);
 	}
@@ -118,14 +163,30 @@ Lx_fs::Notifier::~Notifier()
 
 void Lx_fs::Notifier::entry()
 {
-	struct inotify_event* event { nullptr };
+	struct inotify_event *event { nullptr };
 
-	auto watch_fn = [&] (Entry &elem) {
+	auto modify_fn = [&event] (Entry &elem) {
+		if (elem.watch_fd == event->wd) {
+			elem.notify_all([ ] (Signal_context_capability cap) {
+				Signal_transmitter {cap}.submit();
+			});
+		}
+	};
+
+	auto ignore_fn = [&event, this] (Entry &elem) {
 
 		if (elem.watch_fd == event->wd) {
-			elem.for_each_capability([&] (Cap_entry cap) {
-				Signal_transmitter(cap.cap).submit();
+
+			elem.remove_all(_heap, [ ] (Signal_context_capability cap) {
+
+				/* notify clients something has changed */
+				Signal_transmitter {cap}.submit();
 			});
+
+			/* remove/free watch entry */
+			inotify_rm_watch(_fd, event->wd);
+			_watched_nodes.remove(&elem);
+			destroy(_heap, &elem);
 		}
 	};
 
@@ -137,9 +198,18 @@ void Lx_fs::Notifier::entry()
 		while (pos < length) {
 			event = reinterpret_cast<struct inotify_event*>(&buffer[pos]);
 
+			/* file modified? */
 			if ((event->mask & IN_MODIFY) && !(event->mask & IN_ISDIR)) {
-				for_each(watch_fn);
+				Mutex::Guard guard { _watched_nodes_mutex };
+				for_each(modify_fn);
 			}
+
+			/* watch descriptor no longer watched */
+			else if (event->mask & IN_IGNORED) {
+				Mutex::Guard guard { _watched_nodes_mutex };
+				for_each(ignore_fn);
+			}
+
 			pos += EVENT_SIZE + event->len;
 		}
 	}
@@ -148,6 +218,8 @@ void Lx_fs::Notifier::entry()
 
 int Lx_fs::Notifier::add_watch(const char* path, Signal_context_capability cap)
 {
+	Mutex::Guard guard { _watched_nodes_mutex };
+
 	if (!_watched(path)) {
 		_add_to_watched(path);
 	}
@@ -158,16 +230,26 @@ int Lx_fs::Notifier::add_watch(const char* path, Signal_context_capability cap)
 
 void Lx_fs::Notifier::remove_watch(char const *path, Signal_context_capability cap)
 {
-	auto remove_fn = [&] (Entry &elem) {
+	Mutex::Guard guard { _watched_nodes_mutex };
+
+	auto remove_fn = [&cap, &path, this] (Entry &elem) {
 		if (elem.path == path) {
 
-			elem.for_each_capability([&] (Cap_entry &c) {
-				if (cap == c.cap) {
-					elem.remove_capability(&c);
-				}
-			});
+			elem.remove_capability(_heap, cap);
 		}
 	};
+
+	auto *e { _notify_queue.first() };
+	while (e != nullptr) {
+
+		if (e->cap == cap) {
+			auto *tmp { e };
+			e = e->next();
+			_notify_queue.remove(tmp);
+		} else {
+			e = e->next();
+		}
+	}
 
 	for_each(remove_fn);
 }
